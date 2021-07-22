cc42d69f2a2633860178693ff7409e118db2061b
Louis <louis222220@gmail.com>
2021-07-22 16:54:14 +0800

    Add: credits for link_dotfiles.sh

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Link and overwrite those in $HOME/ with these dotfiles
+# Credits: https://medium.com/@satorusasozaki/manage-your-dotfiles-on-github-8f8a2a57c85
+
 ln -sf $HOME/dotfiles/.zshrc		$HOME/.zshrc
 ln -sf $HOME/dotfiles/.vimrc		$HOME/.vimrc
 ln -sf $HOME/dotfiles/.nvmrc		$HOME/.nvmrc
